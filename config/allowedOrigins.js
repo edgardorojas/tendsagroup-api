eedfc6902784e3feee035d3c8eeef9989c9220b0
Edgardo Rojas <edgardorojas.dev@gmail.com>
2023-08-15 09:17:32 -0600

    Add allowedorigins to ignore

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'http://localhost:3000'
-    // 'https://tendsagroup.onrender.com'
+    // 'http://localhost:3000'
+    'https://tendsagroup.onrender.com'
 ]
 
 module.exports = allowedOrigins
